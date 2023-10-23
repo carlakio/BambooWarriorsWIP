cf3752886b852613e06929ec26d27669ee4c7aaf
PioloVon <piolovreyes@gmail.com>
2023-10-23 21:34:55 +0800

    2nd commit

--- BambooWarriorsWIP/src/main/java/com/example/bamboowarriorswip/MainActivity.java
+++ BambooWarriorsWIP/src/main/java/com/example/bamboowarriorswip/MainActivity.java
@@ -15,6 +15,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton button_Login = findViewById(R.id.button_Login);
+        ImageButton contactUs_btn = findViewById(R.id.contactUs_btn);
+        ImageButton bambooWarriors_btn = findViewById(R.id.bambooWarriors_btn);
+        ImageButton merch_btn = findViewById(R.id.merchandise_btn);
+        ImageButton ourEmblem_btn = findViewById(R.id.ourEmblem_btn);
+
+        contactUs_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent contactUs = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(contactUs);
+            }
+
+        });
+        bambooWarriors_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bambooWarriors = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(bambooWarriors);
+            }
+
+        });
+        merch_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent merchandise = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(merchandise);
+            }
+
+        });
+        ourEmblem_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ourEmblem = new Intent(MainActivity.this, ouremblemdesc.class);
+                startActivity(ourEmblem);
+            }
+
+        });
+
+
+        ImageButton seeMore_btn = findViewById(R.id.seeMore_btn);
+
+        seeMore_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent seeMore = new Intent(MainActivity.this, seemoredesc.class);
+                startActivity(seeMore);
+            }
+
+        });
+
+
 
         button_Login.setOnClickListener(new View.OnClickListener() {
             @Override
